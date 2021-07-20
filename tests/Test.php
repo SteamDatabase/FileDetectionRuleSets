17983bd40f1d290b1e95b300f127c5c25b3f6c33
Pavel Djundik <xPaw@users.noreply.github.com>
2021-07-20 22:22:20 +0300

    Basic test output

--- tests/Test.php
+++ tests/Test.php
@@ -7,9 +7,13 @@ $Detector = new FileDetector( __DIR__ . '/../rules.ini' );
 
 $TestsIterator = new DirectoryIterator( __DIR__ . '/types' );
 
+$FailingTests = [];
+$PassedTests = 0;
+$TotalTestsRun = 0;
+
 foreach( $TestsIterator as $File )
 {
-	if( $File->isDot() || $File->getExtension() !== 'txt' )
+	if( $File->getExtension() !== 'txt' )
 	{
 		continue;
 	}
@@ -21,11 +25,16 @@ foreach( $TestsIterator as $File )
 	{
 		foreach( $TestFilePaths as $Path )
 		{
+			$TotalTestsRun++;
 			$Actual = $Detector->GetMatchingRuleForFilePath( $Path );
 
 			if( $Actual !== null )
 			{
-				throw new \RuntimeException( "Path \"$Path\" returned \"$Actual\"" );
+				$FailingTests[] = "Path \"$Path\" returned \"$Actual\" but it should not have matched anything";
+			}
+			else
+			{
+				$PassedTests++;
 			}
 		}
 
@@ -34,11 +43,30 @@ foreach( $TestsIterator as $File )
 
 	foreach( $TestFilePaths as $Path )
 	{
+		$TotalTestsRun++;
 		$Actual = $Detector->GetMatchingRuleForFilePath( $Path );
 
 		if( $Actual !== $ExpectedType )
 		{
-			throw new \RuntimeException( "Path \"$Path\" does not match \"$ExpectedType\"" );
+			$FailingTests[] = "Path \"$Path\" does not match for \"$ExpectedType\"";
+		}
+		else
+		{
+			$PassedTests++;
 		}
 	}
+}
+
+echo "{$PassedTests} tests out of {$TotalTestsRun} tests passed.\n";
+
+if( !empty( $FailingTests ) )
+{
+	echo "\n" . count( $FailingTests ) . " tests failed:\n";
+
+	foreach( $FailingTests as $Test )
+	{
+		echo $Test . "\n";
+	}
+
+	exit( 1 );
 }
