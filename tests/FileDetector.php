05c8248ef935c6231f2c2d1e72ba85759fc41d64
NintenHero <37460517+MichaelHinrichs@users.noreply.github.com>
2024-01-31 17:05:14 -0600

    Fix indentation

--- tests/FileDetector.php
+++ tests/FileDetector.php
@@ -204,7 +204,7 @@ class FileDetector
 		if( $has( 'Evidence.Unreal' ) )
 		{
 
-      if( $has( 'Evidence.Unreal_1_OR_2' ) )
+			if( $has( 'Evidence.Unreal_1_OR_2' ) )
 			{
 				return 'Engine.Unreal_1_OR_2';
 			}
@@ -212,7 +212,7 @@ class FileDetector
 			if( $has( 'Evidence.Unreal_3' ) )
 			{
 				return 'Engine.Unreal_3';
-      }
+			}
       
 			if( $has( 'Evidence.Unreal_4_OR_5' ) )
 			{
